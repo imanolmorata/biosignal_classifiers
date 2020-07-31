91f56f6c31f89223863fc77e2cc7fc93a387bd8f
amanal <amanal@bitbucket.org>
2020-07-31 13:04:15 +0200

    Created setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+extras = {
+    'complete': [
+        'matplotlib',
+        'numpy',
+        'pandas==1.0.4',
+        'scikit-learn',
+        'scipy',
+        'category_encoders'
+    ]}
+
+setup(name='vgc_adhd',
+      version='0.1',
+      description='Ecosystem for design and implementation of vergence-based classifiers',
+      url='http://github.com/imanolmorata/vergence_adhd_classifier',
+      author='Imanol Morata',
+      author_email='imanol.morata@gmail.com',
+      license='MIT',
+      packages=find_packages(where='source'),
+      python_requires='>=3.6',
+      extras_require=extras,
+      zip_safe=False)
