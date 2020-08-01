7b2dd162e9d7aedb1ec6fc4b3bb7b8c1fb309dbf
amanal <amanal@bitbucket.org>
2020-08-01 11:49:16 +0200

    Wrote docstrings to sampler.py

--- vgc_clf/sampler.py
+++ vgc_clf/sampler.py
@@ -17,6 +17,16 @@ class Sampler:
         self.resample = None
 
     def save_sampling(self, path, sampling_name, mode="pickle"):
+        """
+        Will save a dictionary into a local path with generated batches
+        Args:
+            path: Directory to store the output file.
+            sampling_name: Output file name.
+            mode: pickle or json
+
+        Returns:
+
+        """
 
         assert self.balanced is not None and self.resample is not None, "No sampling has been done yet."
 
@@ -39,6 +49,15 @@ class Sampler:
             raise Exception(f"File save in format {mode} not implemented yet.")
 
     def load_sampling(self, path, sampling_name):
+        """
+        Will attempt to load an exsiting sampling from a local file.
+        Args:
+            path: Path to file.
+            sampling_name: File name without extension.
+
+        Returns:
+
+        """
 
         assert os.path.exists(path), f"Directory {path} not found."
 
@@ -58,6 +77,17 @@ class Sampler:
         self.n_batches = len(input_dictionary["batches"].keys())
 
     def _get_data_frame_info(self, df, target_variable, input_variables=None):
+        """
+        In the event that a new sampling is created from a pandas.DataFrame, this will read the samling object
+        information that relates to it.
+        Args:
+            df: pandas.DataFrame from which to sample.
+            target_variable: User-defined target variable.
+            input_variables: User-defined input variable list. If None, will simply take df column names.
+
+        Returns:
+
+        """
 
         assert target_variable in list(df.columns), "Target variable not present in data frame columns."
 
@@ -76,11 +106,27 @@ class Sampler:
 
     def generate_batches(self, df, n_batches, batch_len, target_variable, input_variables=None, balanced=True,
                          resample=True, verbose=True):
+        """
+        Generates batches from a pandas.DataFrame.
+        Args:
+            df: pandas.DataFrame from which to sample.
+            n_batches: Total number of batches to generate.
+            batch_len: Length of each batch.
+            target_variable: User-defined target variable.
+            input_variables: User-defined input variable list. If None, will simply take df column names.
+            balanced: Whether to balance the batches by target_variable.
+            resample: Whether to sample batches with replacement.
+            verbose: Whether to print progress on screen.
+
+        Returns:
+
+        """
 
         self._get_data_frame_info(df=df, target_variable=target_variable, input_variables=input_variables)
 
         self.balanced = balanced
         self.resample = resample
+        self.batches = {}
 
         for i in np.arange(n_batches):
 
@@ -91,16 +137,26 @@ class Sampler:
                 _df_0 = self.df[self.df[self.target_variable] == 0]
                 _df_1 = self.df[self.df[self.target_variable] == 1]
 
-                _s0 = np.random.choice(_df_0.index, size=int(batch_len / 2), replace=True)
-                _s1 = np.random.choice(_df_1.index, size=int(batch_len / 2), replace=True)
+                if resample:
+                    max_len = int(batch_len / 2)
+                else:
+                    max_len = np.min([len(_df_0), len(_df_1), int(batch_len / 2)])
+
+                _s0 = np.random.choice(_df_0.index, size=max_len, replace=resample)
+                _s1 = np.random.choice(_df_1.index, size=max_len, replace=resample)
 
                 _sX = self.df.loc[np.hstack((_s0, _s1)), self.input_variables]
                 _sy = self.df.loc[np.hstack((_s0, _s1)), self.target_variable]
             else:
-                _s0 = np.random.choice(self.df.index, size=batch_len, replace=resample)
+                if resample:
+                    max_len = batch_len
+                else:
+                    max_len = np.min([batch_len, len(self.df)])
 
-                _sX = self.df.loc[_s0, self.input_variables]
-                _sy = self.df.loc[_s0, self.target_variable]
+                _sS = np.random.choice(self.df.index, size=max_len, replace=resample)
+
+                _sX = self.df.loc[_sS, self.input_variables]
+                _sy = self.df.loc[_sS, self.target_variable]
 
             _batch = {
                 "X": np.array(_sX),
@@ -118,6 +174,16 @@ class Sampler:
         print("Batch sampling complete.", flush=True)
 
     def extract_batch(self, index, y=False):
+        """
+        Retrieve the numpy.ndarray stored in a particular batch.
+        Args:
+            index: Index of the particular batch to extract.
+            y: If true, will retrieve the target vector.
+
+        Returns:
+            numpy.ndarray containing the batch.
+
+        """
 
         assert index < self.n_batches, "Given index is out of bounds"
 
@@ -129,6 +195,16 @@ class Sampler:
             return to_extract["X"]
 
     def combine_samplings(self, new_sampling):
+        """
+        Adds the batches present in a second sampling to current sampling object. The second sampling must have been
+        generated with the same input and output variables. Note that this will only check that the names match, the
+        user will still have to make sure that the data is compatible or makes sense.
+        Args:
+            new_sampling: Sampling object containing the batches to add.
+
+        Returns:
+
+        """
 
         assert type(new_sampling) == Sampler
         assert new_sampling.target_variable == self.target_variable, "Target variable names do not match."
