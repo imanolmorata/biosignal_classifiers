eb8110d82bf35d4f195cc1278ecb1c7465de9f50
amanal <amanal@bitbucket.org>
2020-08-02 16:20:36 +0200

    Came up with a better strcuture

--- vgc_clf/tasks/classifier.py
+++ vgc_clf/tasks/classifier.py
@@ -7,7 +7,7 @@ from sklearn.linear_model import RidgeClassifier as rc
 
 from vgc_clf.sampler import Sampler
 from vgc_clf.ensemble import Ensemble
-from vgc_clf.utils import DataFrameUtils as DfUtils
+from vgc_clf.utils import data_frame_utils as df_utils
 
 warnings.filterwarnings(action="ignore")
 
@@ -30,52 +30,59 @@ trial_encoders = ["condition", "side", "target", "correct_response"]
 signal_variables = [col for col in df.columns if "feat_" in col][210:]
 side_variables = ["trial_num", "sti_time", "reaction_time", "red_frog_time", "broken_fixations",
                   "total_session_time", "age", "sex", "oculomotor_deficiences", "school_performance"]
-target_variables = ["diagnosis"]
+target_variable = "diagnosis"
 
-df_dgn = DfUtils.get_patients_data_frame(df=df, patient_column_name="patient",
-                                         patient_info_columns=["patient", "group", "age", "sex",
-                                                               "oculomotor_deficiences", "school_performance",
-                                                               "diagnosis"])
+df_dgn = df_utils.get_patients_data_frame(df=df, patient_column_name="patient",
+                                          patient_info_columns=["patient", "group", "age", "sex",
+                                                                "oculomotor_deficiences", "school_performance",
+                                                                "diagnosis"])
 
-df_val, df_fit, train_index, _ = DfUtils.get_train_validation_from_data_frame(signal_df=df, patients_df=df_dgn,
-                                                                              patient_id_column="patient",
-                                                                              strata="group",
-                                                                              strata_delimiter="ADHD", test_size=10)
+cv_batches = 10
+cv_dfs = df_utils.generate_cross_validation_batch(n_batches=cv_batches, signal_df=df, patients_df=df_dgn,
+                                                  patient_id_column="patient",
+                                                  strata="group",
+                                                  strata_delimiter="ADHD", test_size=10)
 
-patients_fit = list(df_dgn.loc[train_index, "patient"])
+for k, (df_fit, df_val, train_index, _) in enumerate(cv_dfs):
 
-train_patients = np.random.choice(patients_fit, size=int(np.ceil(0.8 * len(patients_fit))), replace=False)
-test_patients = list(set(patients_fit) - set(train_patients))
+    print(f"------ITERATION {k + 1} of {cv_batches}", flush=True)
 
-df_train = df_fit[df_fit.patient.isin(train_patients)].copy()
-df_test = df_fit[df_fit.patient.isin(test_patients)].copy()
+    patients_fit = list(df_dgn.loc[train_index, "patient"])
 
-train_samplings = Sampler()
-test_samplings = Sampler()
+    train_patients = np.random.choice(patients_fit, size=int(np.ceil(0.8 * len(patients_fit))), replace=False)
+    test_patients = list(set(patients_fit) - set(train_patients))
 
-valid_variables = trial_encoders + signal_variables + side_variables
+    df_train = df_fit[df_fit.patient.isin(train_patients)].copy()
+    df_test = df_fit[df_fit.patient.isin(test_patients)].copy()
 
-train_samplings.generate_batches(df_train,
-                                 n_batches=100,
-                                 batch_len=1600,
-                                 target_variable="diagnosis",
-                                 input_variables=valid_variables,
-                                 verbose=True)
+    train_samplings = Sampler()
+    test_samplings = Sampler()
 
-test_samplings.generate_batches(df_test,
-                                n_batches=100,
-                                batch_len=400,
-                                target_variable="diagnosis",
-                                input_variables=valid_variables,
-                                verbose=True)
+    valid_variables = trial_encoders + signal_variables + side_variables
 
-classifier_list = [rc, tree]
-node_sizes = [750, 250]
-kwargs_list = [{"alpha": 1.2},
-               {"max_depth": 30, "max_features": 30, "min_samples_leaf": 30}]
+    train_samplings.generate_batches(df_train,
+                                     n_batches=100,
+                                     batch_len=1600,
+                                     target_variable=target_variable,
+                                     input_variables=valid_variables,
+                                     verbose=True)
 
-vgc_classifier = Ensemble(classifier_list=classifier_list, node_sizes=node_sizes, kwargs_list=kwargs_list)
-vgc_classifier.fit(batch_list_train=train_samplings, batch_list_test=test_samplings,
-                   score_cap=0.6, get_best=100, verbose=True)
+    test_samplings.generate_batches(df_test,
+                                    n_batches=100,
+                                    batch_len=400,
+                                    target_variable=target_variable,
+                                    input_variables=valid_variables,
+                                    verbose=True)
 
-prd = vgc_classifier.predict(df=df_val, verbose=True)
+    classifier_list = [rc, tree]
+    node_sizes = [750, 250]
+    kwargs_list = [{"alpha": 1.2},
+                   {"max_depth": 30, "max_features": 30, "min_samples_leaf": 30}]
+
+    vgc_classifier = Ensemble(classifier_list=classifier_list, node_sizes=node_sizes, kwargs_list=kwargs_list)
+    vgc_classifier.fit(batch_list_train=train_samplings, batch_list_test=test_samplings,
+                       score_cap=0.6, get_best=100, verbose=True)
+
+    prd = vgc_classifier.predict(df=df_val, verbose=True)
+
+    print(f"---SCORE: {(prd == df_val[target_variable]).mean()}", flush=True)
---
+++ vgc_clf/utils/data_frame_utils.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+
+
+def get_patients_data_frame(df, patient_column_name="patient", patient_info_columns=None):
+    assert patient_column_name in list(df.columns), "Patient column not found in data frame."
+
+    if patient_info_columns is None:
+        patient_info_columns = list(df.columns)
+    if patient_column_name not in patient_info_columns:
+        patient_info_columns = [patient_column_name] + patient_info_columns
+
+    df_patients = df[patient_info_columns].groupby(patient_column_name, as_index=False).max()
+
+    return df_patients
+
+
+def get_train_validation_from_data_frame(signal_df, patients_df, patient_id_column, strata, strata_delimiter,
+                                         test_size):
+    condition = patients_df[strata] == strata_delimiter
+    max_len = np.min([len(patients_df[~condition]), len(patients_df[condition])])
+
+    if test_size > max_len:
+        test_size = max_len
+
+    negative_index = np.random.choice(patients_df[~condition].index, size=test_size)
+    positive_index = np.random.choice(patients_df[condition].index, size=test_size)
+
+    index_val = pd.Index(list(negative_index) + list(positive_index))
+    index_fit = pd.Index(set(patients_df.index) - set(index_val))
+
+    signal_val = signal_df.merge(patients_df.loc[index_val, patient_id_column], on=patient_id_column, how="right")
+    signal_fit = signal_df.merge(patients_df.loc[index_fit, patient_id_column], on=patient_id_column, how="right")
+
+    return signal_fit, signal_val, index_fit, index_val
+
+
+def generate_cross_validation_batch(n_batches, signal_df, patients_df, patient_id_column, strata, strata_delimiter,
+                                    test_size):
+    for _ in np.arange(n_batches):
+        yield get_train_validation_from_data_frame(signal_df=signal_df,
+                                                   patients_df=patients_df,
+                                                   patient_id_column=patient_id_column,
+                                                   strata=strata,
+                                                   strata_delimiter=strata_delimiter,
+                                                   test_size=test_size)
