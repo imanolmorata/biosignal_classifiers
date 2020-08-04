c3351035a740a19274f69b82766174b29094ed71
amanal <amanal@bitbucket.org>
2020-08-04 16:11:42 +0200

    Added data set transformation to cross validation experiment

--- vgc_clf/tasks/cross_validation_experiment.py
+++ vgc_clf/tasks/cross_validation_experiment.py
@@ -6,27 +6,39 @@ import warnings
 
 from sklearn.metrics import roc_auc_score
 
-from vgc_clf.sampler.sampler import Sampler
+from vgc_clf.dataset_transformer.encoder import Encoder
+from vgc_clf.dataset_transformer.normalizer import Normalizer
+from vgc_clf.dataset_transformer.signal_compressor import SignalCompressor as sg_com
 from vgc_clf.ensemble.ensemble import Ensemble
+from vgc_clf.sampler.sampler import Sampler
 from vgc_clf.utils import data_frame_utils as df_utils
 from vgc_clf.utils import ensemble_utils as ens_utils
+from vgc_clf.utils import transformer_utils as trf_utils
+from vgc_clf.utils import compressor_utils as cm_utils
 
 warnings.filterwarnings(action="ignore")
 
 
 def run_cross_validation_experiment(df, cv_batches, subject_dictionary, sampler_dictionary, ensemble_dictionary,
-                                    test_set_size=10, verbose=False):
+                                    transformer_dictionary, test_set_size=10, verbose=False):
+    """
+    Run a simple cross-validation experiment.
+    Args:
+        df:
+        cv_batches:
+        subject_dictionary:
+        sampler_dictionary:
+        ensemble_dictionary:
+        transformer_dictionary:
+        test_set_size:
+        verbose:
+
+    Returns:
+
+    """
     subject_column = subject_dictionary["subject_id_column"]
     subject_info = subject_dictionary["subject_data_columns"]
     subject_target = subject_dictionary["target_variable"]
-
-    df_dgn = df_utils.get_subjects_data_frame(df=df, subject_column_name=subject_column,
-                                              subject_info_columns=subject_info)
-
-    cv_dfs = df_utils.generate_cross_validation_batch(n_batches=cv_batches, signal_df=df, subjects_df=df_dgn,
-                                                      subject_id_column=subject_column,
-                                                      target_variable=subject_target,
-                                                      test_size=test_set_size)
 
     fraction = sampler_dictionary["train_test_fraction"]
     valid_variables = sampler_dictionary["input_variables"]
@@ -38,14 +50,57 @@ def run_cross_validation_experiment(df, cv_batches, subject_dictionary, sampler_
 
     classifier_list = [clf for clf in ens_utils.get_classifier_objects(ensemble_dictionary["classifier_list"])]
     node_sizes = ensemble_dictionary["node_sizes"]
-    kwargs_list = ensemble_dictionary["kwargs_list"]
+    classifier_kwargs_list = ensemble_dictionary["kwargs_list"]
     score_cap = ensemble_dictionary["score_cap"]
     get_best = ensemble_dictionary["get_best"]
     class_threshold = ensemble_dictionary["class_threshold"]
 
+    encoder_list = [enc for enc in trf_utils.get_transformer_objects(transformer_dictionary["Encoders"])]
+    encoder_kwargs = transformer_dictionary["Encoders_kwargs"]
+    encoders_input_columns = transformer_dictionary["Encoders_input_columns"]
+    encoders_target_columns = transformer_dictionary["Encoders_target_columns"]
+
+    normalizers_list = [nrm for nrm in trf_utils.get_transformer_objects(transformer_dictionary["Normalizers"])]
+    normalizers_kwargs = transformer_dictionary["Normalizers_kwargs"]
+    normalizers_input_columns = transformer_dictionary["Normalizers_input_columns"]
+
+    signal_compressors_clusters = transformer_dictionary["Signal_compressor_clusters"]
+    signal_compressor_input_columns = transformer_dictionary["Signal_compressor_input_columns"]
+    signal_compressor_apply_functions = \
+        [ap for ap in cm_utils.get_apply_functions(transformer_dictionary["signal_compressor_apply_functions"])]
+
+    df_dgn = df_utils.get_subjects_data_frame(df=df, subject_column_name=subject_column,
+                                              subject_info_columns=subject_info)
+
+    cv_dfs = df_utils.generate_cross_validation_batch(n_batches=cv_batches, signal_df=df, subjects_df=df_dgn,
+                                                      subject_id_column=subject_column,
+                                                      target_variable=subject_target,
+                                                      test_size=test_set_size)
+
     scores = []
     for k, (df_fit, df_val, train_index, _) in enumerate(cv_dfs):
         print(f"------ITERATION {k + 1} of {cv_batches}", flush=True)
+
+        if len(encoder_list) > 0:
+            encoder_obj = Encoder(transformer_list=encoder_list,
+                                  kwargs_list=encoder_kwargs,
+                                  input_cols_list=encoders_input_columns,
+                                  target_col_list=encoders_target_columns)
+            df_fit = encoder_obj.fit_transform(df=df_fit, verbose=verbose)
+            df_val = encoder_obj.transform(df=df_val, verbose=verbose)
+        if len(normalizers_list) > 0:
+            normalizer_obj = Normalizer(transformer_list=normalizers_list,
+                                        kwargs_list=normalizers_kwargs,
+                                        input_cols_list=normalizers_input_columns)
+            df_fit = normalizer_obj.fit_transform(df=df_fit, verbose=verbose)
+            df_val = normalizer_obj.transform(df=df_val, verbose=verbose)
+        if len(signal_compressors_clusters) > 0:
+            compressor_obj = sg_com(n_clusters_list=signal_compressors_clusters,
+                                    input_cols_list=signal_compressor_input_columns,
+                                    apply_estimator_list=signal_compressor_apply_functions).fit(df=df_fit,
+                                                                                                verbose=verbose)
+            df_fit = compressor_obj.transform(df=df_fit, verbose=verbose)
+            df_val = compressor_obj.transform(df=df_val, verbose=verbose)
 
         ts = int(np.ceil((1. - fraction) * len(train_index)))
         df_train, df_test, _, _ = df_utils.get_train_validation_from_data_frame(signal_df=df_fit,
@@ -71,7 +126,7 @@ def run_cross_validation_experiment(df, cv_batches, subject_dictionary, sampler_
                                         input_variables=valid_variables,
                                         verbose=verbose)
 
-        vgc_classifier = Ensemble(classifier_list=classifier_list, node_sizes=node_sizes, kwargs_list=kwargs_list)
+        vgc_classifier = Ensemble(classifier_list=classifier_list, node_sizes=node_sizes, kwargs_list=classifier_kwargs_list)
         vgc_classifier.fit(batch_list_train=train_samplings, batch_list_test=test_samplings,
                            score_cap=score_cap, get_best=get_best, verbose=verbose)
 
@@ -109,6 +164,8 @@ if __name__ == "__main__":
                         required=True)
     parser.add_argument("-e", "--ensemble_json", help="Path to json file containing ensemble generation info.",
                         type=str, required=True)
+    parser.add_argument("-t", "--transformer_json", help="Path to json file containing transformer generation info.",
+                        type=str, required=True)
     parser.add_argument("--verbose", help="Be verbose on progress on screen", default=False, action="store_true")
 
     args = parser.parse_args()
@@ -117,10 +174,12 @@ if __name__ == "__main__":
     subject_dict = json.load(open(args.subject_json, "r"))
     sampler_dict = json.load(open(args.sampler_json, "r"))
     ensemble_dict = json.load(open(args.ensemble_json, "r"))
+    transformer_dict = json.load(open(args.transformer_json, "r"))
 
     run_cross_validation_experiment(df=df_in,
                                     cv_batches=args.n_batches,
                                     subject_dictionary=subject_dict,
                                     sampler_dictionary=sampler_dict,
                                     ensemble_dictionary=ensemble_dict,
+                                    transformer_dictionary=transformer_dict,
                                     verbose=args.verbose)
