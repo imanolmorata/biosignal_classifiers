54669d06b4c94b361357ad3e4fc91dc8b9fece60
imanol <imanol.morata@gmail.com>
2020-08-06 14:07:18 +0200

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ extras = {
     ]}
 
 setup(name='bio-signal-binary-classifier',
-      version='0.0',
+      version='0.1',
       description='Ecosystem for design and implementation of bio-signals-based binary classifiers',
       url='http://github.com/imanolmorata/biosignal_classifier',
       author='Imanol Morata',
