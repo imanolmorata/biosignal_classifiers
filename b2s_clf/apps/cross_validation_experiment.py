10b0fd8828b5b7685f7023786ea517c8ece3637c
imanol <imanol.morata@gmail.com>
2020-08-06 16:46:50 +0200

    Added save_experiments method, adapted apps and updated requirements and documentation.

--- b2s_clf/apps/cross_validation_experiment.py
+++ b2s_clf/apps/cross_validation_experiment.py
@@ -10,10 +10,11 @@ The app returns accuracy, FPR, FNR and ROC AUC score.
 
 Example call:
     python cross_validation_experiment.py -b 10 -p subject.json -s sampling.json -e ensemble.json -t transform.json
-    -z 15 --verbose
+    -z 15  --save_experiment /home/my_path --verbose
 """
 
 import argparse
+import datetime
 import json
 import pandas as pd
 import warnings
@@ -40,6 +41,8 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--transformer_json", help="Path to json file containing transformer generation info.",
                         type=str, required=True)
     parser.add_argument("-z", "--test_set_size", help="Class-wise test size.", type=int, required=False, default=10)
+    parser.add_argument("--save_experiment", help="Path to save experiment results.", type=str, required=False,
+                        default=None)
     parser.add_argument("--verbose", help="Be verbose on progress on screen", default=False, action="store_true")
 
     args = parser.parse_args()
@@ -52,4 +55,11 @@ if __name__ == "__main__":
 
     experiment_object = CrossValidationExperiment(df_in, subject_dict, sampler_dict, ensemble_dict, transformer_dict)
     experiment_object.run(cv_batches=args.n_batches, test_set_size=args.test_set_size, verbose=args.verbose)
-    print(experiment_object.experiment_stats)
+
+    if args.save_experiment is not None:
+        today_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        exp_name = f"{'_'.join(experiment_object.experiment_type).split(' ')}_{today_date}"
+        experiment_object.save_experiment_results(experiment_name=exp_name, experiment_path=args.save_experiment)
+    else:
+        print("\nEXPERIMENT RESULTS")
+        print(experiment_object.experiment_stats.mean())
